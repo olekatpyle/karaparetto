cca2c3e0fa78cef03df7703e40f825c33670b9d1
Oliver Brewka <pymeonlee@gmail.com>
2022-08-03 15:39:49 +0200

    added static layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,30 @@
-export default function Home() {
-    return <div className="text-4xl">KARAPARETTO</div>;
+export default function Page() {
+    return (
+        <div className="">
+            <nav className="w-1/4 h-screen sticky float-left"></nav>
+            <div className="grid grid-flow-row">
+                <div className=" grid grid-cols-4 gap-4  place-items-center ">
+                    <div className="box-content w-48 h-48 bg-white">1</div>
+                    <div className="box-content w-48 h-48 bg-white">2</div>
+                    <div className="box-content w-48 h-48 bg-white">3</div>
+                    <div className="box-content w-48 h-48 bg-white">4</div>
+                    <div className="box-content w-48 h-48 bg-white">1</div>
+                    <div className="box-content w-48 h-48 bg-white">2</div>
+                    <div className="box-content w-48 h-48 bg-white">3</div>
+                    <div className="box-content w-48 h-48 bg-white">4</div>
+                    <div className="box-content w-48 h-48 bg-white">1</div>
+                    <div className="box-content w-48 h-48 bg-white">2</div>
+                    <div className="box-content w-48 h-48 bg-white">3</div>
+                    <div className="box-content w-48 h-48 bg-white">4</div>
+                    <div className="box-content w-48 h-48 bg-white">1</div>
+                    <div className="box-content w-48 h-48 bg-white">2</div>
+                    <div className="box-content w-48 h-48 bg-white">3</div>
+                    <div className="box-content w-48 h-48 bg-white">4</div>
+                </div>
+                <div className="flex items-center justify-center">
+                    <button className="my-28">Shuffle</button>
+                </div>
+            </div>
+        </div>
+    );
 }

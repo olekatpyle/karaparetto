fed2e306521cc84fb94d44693a394e154d0ca178
Oliver Brewka <pymeonlee@gmail.com>
2022-08-03 15:43:29 +0200

    added layout header

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,10 @@ export default function Page() {
         <div className="">
             <nav className="w-1/4 h-screen sticky float-left"></nav>
             <div className="grid grid-flow-row">
-                <div className=" grid grid-cols-4 gap-4  place-items-center ">
+                <div className="text-4xl mt-4 flex items-center justify-center">
+                    KARAPARETTO
+                </div>
+                <div className="mt-4 grid grid-cols-4 gap-4  place-items-center ">
                     <div className="box-content w-48 h-48 bg-white">1</div>
                     <div className="box-content w-48 h-48 bg-white">2</div>
                     <div className="box-content w-48 h-48 bg-white">3</div>
@@ -22,7 +25,7 @@ export default function Page() {
                     <div className="box-content w-48 h-48 bg-white">4</div>
                 </div>
                 <div className="flex items-center justify-center">
-                    <button className="my-28">Shuffle</button>
+                    <button className="my-28 text-2xl">Shuffle</button>
                 </div>
             </div>
         </div>

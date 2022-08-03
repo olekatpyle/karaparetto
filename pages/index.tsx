322fb396dbbe9cc2d0b899be5564f5361523ef27
Oliver Brewka <pymeonlee@gmail.com>
2022-08-03 16:26:48 +0200

    removed margins, instead using spacer elements

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,10 +4,12 @@ export default function Page() {
             <nav className="w-1/4 h-screen sticky float-left"></nav>
             <nav className="w-1/4 h-screen sticky float-right"></nav>
             <div className="grid grid-flow-row">
-                <div className="text-4xl my-8 flex items-center justify-center">
+                <div className="p-4"></div>
+                <div className="text-4xl flex items-center justify-center">
                     KARAPARETTO
                 </div>
-                <div className="mb-4 grid grid-cols-4 gap-10  place-items-center ">
+                <div className="p-4"></div>
+                <div className="grid grid-cols-4 gap-10  place-items-center ">
                     <div className="box-content w-48 h-48 bg-white">1</div>
                     <div className="box-content w-48 h-48 bg-white">2</div>
                     <div className="box-content w-48 h-48 bg-white">3</div>
@@ -25,8 +27,9 @@ export default function Page() {
                     <div className="box-content w-48 h-48 bg-white">3</div>
                     <div className="box-content w-48 h-48 bg-white">4</div>
                 </div>
+                <div className="p-8"></div>
                 <div className="flex items-center justify-center">
-                    <button className="my-14 text-2xl">Shuffle</button>
+                    <button className="text-2xl">Shuffle</button>
                 </div>
             </div>
         </div>

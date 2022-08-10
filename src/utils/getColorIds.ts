38f22c39f26fab6f7c938351e7987c684e306bc8
Oliver Brewka <pymeonlee@gmail.com>
2022-08-10 03:02:25 +0200

    set MAX_COLOR_ID to value of main

--- src/utils/getColorIds.ts
+++ src/utils/getColorIds.ts
@@ -1,4 +1,4 @@
-const MAX_ID = 1000000;
+const MAX_ID = 2936265;
 
 export const getRandomId: (colorIds: number[]) => number = (colorIds) => {
   const id: number = Math.floor(Math.random() * MAX_ID);
